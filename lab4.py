d7455af37cfaa1161fa3feaad639eff6f6c0f0c4
Dominick <dominickhera@me.com>
2015-10-09 10:01:26 -0400

    added lab

---
+++ lab4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+print "Enter 3 numbers scrub"
+number = raw_input()
+secondNumber = raw_input()
+thirdNumber = raw_input()
+exitVar = raw_input()
+print number
+print secondNumber
+print thirdNumber
+print exitVar
