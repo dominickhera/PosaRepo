5c8f212c3dab0b873058ab95e7f378dde89b830e
Dominick <dominickhera@me.com>
2015-10-13 20:25:16 -0400

    added dice function

--- piggehBackUp.c
+++ piggehBackUp.c
@@ -7,6 +7,63 @@
 #include<stdlib.h>
 #include<time.h>
 
+void showDice(int num) {
+switch(num) {
+	case 0:
+		printf("how about no");
+		break;	
+
+	case 1:
+		printf("+-------+\n");
+		printf("|       |\n");
+		printf("|   o   |\n");
+		printf("|       |\n");
+		printf("+-------+\n");
+		break;
+	case 2:
+		printf("+-------+\n");
+                printf("|       |\n");
+                printf("| o   o |\n");
+                printf("|       |\n");
+                printf("+-------+\n");
+		break;
+	case 3:
+		printf("+-------+\n");
+                printf("| o     |\n");
+                printf("|   o   |\n");
+                printf("|     o |\n");
+                printf("+-------+\n");
+                break;
+
+	case 4:
+		printf("+-------+\n");
+                printf("| o   o |\n");
+                printf("|       |\n");
+                printf("| o   o |\n");
+                printf("+-------+\n");
+                break;
+
+	case 5:
+		printf("+-------+\n");
+                printf("| o   o |\n");
+                printf("|   o   |\n");
+                printf("| o   o |\n");
+                printf("+-------+\n");
+                break;
+	case 6:
+		printf("+-------+\n");
+                printf("| o   o |\n");
+                printf("| o   o |\n");
+                printf("| o   o |\n");
+                printf("+-------+\n");
+                break;
+
+	default:
+		printf("default answer");
+		break;
+}
+}
+
 int main(void) {
 
 srand ((int)time(0));
@@ -21,15 +78,15 @@ int randomNumber = 0;
 printf("\nwelcome to a game  of pigthrones\n");
 printf("---------------------------\n");
 printf("  made by dominick hera   \n");
-printf("\ndo you and your friend want to play a game? (y/n) :");
+printf("\ndo you and your friend want to play a game? (y/n) \n >");
 scanf("%c", &charAnswer);
 
 if (charAnswer == 'y') {
-printf("please set the game total: \n");
+printf("please set the game total: \n >");
 scanf("%d", &gameTotal);
 
 
-while (playerOneTotal <= gameTotal || playerTwoTotal <= gameTotal ) {
+while ((playerOneTotal < (gameTotal - 1)) || (playerTwoTotal < (gameTotal - 1))) {
 printf("do you want to (r)oll or (h)old? : \n >");
 
 scanf("\n%c", &rollAnswer);
@@ -37,7 +94,6 @@ scanf("\n%c", &rollAnswer);
 if (rollAnswer == 'r') {
 
 randomNumber = ((rand() % 6) + 1);
-printf("%d\n", randomNumber);
 
 switch (randomNumber) {
 	case 0:
@@ -47,21 +103,27 @@ switch (randomNumber) {
 
 	case 1:
 		printf("You rolled a %d\n", randomNumber);
+		printf("--------------------\n");
+		showDice(randomNumber);
+		printf("--------------------\n");
 		if (playerTurn == 1) {
-		playerOneTotal = playerOneTotal + randomNumber;
 		printf("Player %d Point Total: %d\n", playerTurn, playerOneTotal);
+		printf("Player 2 Point Total: %d\n", playerTwoTotal);
+		printf("Game Total: %d\n", gameTotal);
 		} else {
-		playerTwoTotal = playerTwoTotal + randomNumber;
-		printf("Player %d Point Total: %d\n", playerTurn, playerTwoTotal);
+		printf("Player 1 Point Total: %d\n", playerOneTotal);
+		printf("Player %d Point Total: %d\n", playerTurn,  playerTwoTotal);
+		printf("Game Total: %d\n", gameTotal);
 		}
 		if (playerOneTotal >= gameTotal) {
-printf("Player one has won! weo weo weo\n");
-return 0;
-} else 
- if (playerTwoTotal >= gameTotal) {
-printf("Player two has won! weo weo weo\n");
-return 0;
-}
+		printf("player one has won! weo weo weo\n");
+		return 0;
+		} else 
+ 		if (playerTwoTotal >= gameTotal) {
+		printf("player two has won! weo weo weo\n");
+		return 0;
+		}
+		printf("-------------------\n");
 		printf("Sorry, you lose a turn\n");
 		if (playerTurn == 1) {
 		playerTurn = 2;
@@ -70,6 +132,7 @@ return 0;
 		playerTurn = 1;
 		printf("it is now Player %d's turn\n", playerTurn);
 		}
+		printf("-----------------------\n");
 		break;
 
 
@@ -79,12 +142,29 @@ return 0;
 	case 5:
 	case 6:
 		printf("You rolled %d\n", randomNumber);
+		printf("-----------------------\n");
+		showDice(randomNumber);
+		printf("-----------------------\n");
 		if (playerTurn == 1) {
 		playerOneTotal = playerOneTotal + randomNumber;
 		printf("Player %d Point Total: %d\n", playerTurn, playerOneTotal);
+		printf("Player 2 Point Total: %d\n", playerTwoTotal);
+		printf("Game Total: %d\n", gameTotal);
+		printf("---------------------------\n");
 		} else {
 		playerTwoTotal = playerTwoTotal + randomNumber;
-		printf("Player %d Point Total %d\n", playerTurn, playerTwoTotal);
+		printf("Player 1 Point Total %d\n", playerOneTotal);
+		printf("Player %d  Point Total %d\n", playerTurn,  playerTwoTotal);
+		printf("Game Total: %d\n", gameTotal);
+		printf("---------------------------\n");
+		if (playerOneTotal >= gameTotal) {
+		printf("player one has won! weo weo weo\n");
+		return 0;
+		} else
+		if (playerTwoTotal >= gameTotal) {
+		printf("player two has won! weo weo weo\n");
+		return 0;
+		}
 		}
 		break;
 
@@ -94,18 +174,23 @@ return 0;
 } else
  if (rollAnswer == 'h') {
 if (playerOneTotal >= gameTotal) {
-printf("Player one wins! weo weo weo \n");
+printf("player one wins! weo weo weo \n");
 return 0;
 } else
  if (playerTwoTotal >= gameTotal) {
-printf("Player two wins! weo weo weo \n");
+printf("player two wins! weo weo weo \n");
 return 0;
 } else {
-printf("it's now the next players turn");
+if (playerTurn == 1) {
+playerTurn = 2;
+} else { 
+playerTurn = 1;
+}
+printf("it's now players %d turn\n", playerTurn);
+printf("---------------------------\n");
 }
 }
 }
 }
 return 0;
-
 }
