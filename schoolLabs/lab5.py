6e753c90507864dcc26d30479e6af308a1746936
Dominick <dominickhera@me.com>
2015-11-06 01:47:25 -0500

    did lab5 for tomorrow

---
+++ schoolLabs/lab5.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+for i in range (1,6):
+   for j in range (i, i * 6, i):
+      print j, '    ',
+   print '\n'
--- schoolLabs/testPython.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-number = raw_input()
-print number
