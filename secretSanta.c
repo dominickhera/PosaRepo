9c27ddfadb031c1b76728ed03749d899aee0684e
dominickhera <dominickhera@Dominicks-MacBook-Pro.local>
2015-11-18 22:31:32 -0500

    commented on code, attempted to fix segmentation error to no luck

--- secretSanta.c
+++ secretSanta.c
@@ -7,25 +7,35 @@ int main(void) {
 srand(time(NULL));
 int i = 0;
 int randomNumber = 0;
+int personFind = 0;
 int numberPeople;
 
+//sets size of array
 printf("how many people will there be?\n");
 scanf("%d", &numberPeople);
-char peopleArray[numberPeople];
-char secondArray[numberPeople];
+char *peopleArray[numberPeople];
+char *secondArray[numberPeople];
 
+//SHOULD assign names to each number in array
 for (i = 0; i < numberPeople; i++) {
 printf("input persons name: ");
-scanf("%s", peopleArray);
-strcpy(peopleArray, secondArray);
+scanf("%s", peopleArray[i]);
 }
+
+//transition phrase w000000
 printf("alright, next lets find you your secret santa is!\n");
 
+
+//hypothetically should match users who enter their name with another person//
+//in the list and then prevent that person from picking the same person or //
+//being picked again.//
 for (i = 0; i < numberPeople; i++) {
 printf("enter your name: ");
 scanf("%s", secondArray);
 randomNumber = ((rand() % numberPeople) + 1);
-printf("%s", peopleArray[randomNumber]);
+personFind = randomNumber - 1;
+printf("%d", randomNumber);
+printf("%s", peopleArray[personFind]);
 /*
 if (secondArray == people Array) {
 
