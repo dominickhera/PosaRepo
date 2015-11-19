0ee00e3f6dfb18e06643b2d236d3c3982c196335
Dominick <dominickhera@me.com>
2015-11-18 19:11:03 -0500

    small chagnes to secret santa

--- secretSanta.c
+++ secretSanta.c
@@ -11,8 +11,8 @@ int numberPeople;
 
 printf("how many people will there be?\n");
 scanf("%d", &numberPeople);
-char peopleArray[numberPeople - 1];
-char secondArray[numberPeople - 1];
+char peopleArray[numberPeople];
+char secondArray[numberPeople];
 
 for (i = 0; i < numberPeople; i++) {
 printf("input persons name: ");
@@ -24,7 +24,7 @@ printf("alright, next lets find you your secret santa is!\n");
 for (i = 0; i < numberPeople; i++) {
 printf("enter your name: ");
 scanf("%s", secondArray);
-randomNumber = (rand() % numberPeople);
+randomNumber = ((rand() % numberPeople) + 1);
 printf("%s", peopleArray[randomNumber]);
 /*
 if (secondArray == people Array) {
