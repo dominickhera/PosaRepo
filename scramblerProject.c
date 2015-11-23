33dfb783d74c6031d44e85e71a188fa7c6d2b90f
Dominick <dominickhera@me.com>
2015-11-23 18:42:19 -0500

    more work on scrambler

--- scramblerProject.c
+++ scramblerProject.c
@@ -2,7 +2,7 @@
 #include<string.h>
 #include<time.h>
 
-int getFileName(char docName[50]) {
+/*int getFileName(char docName) {
 FILE *ifp;
 
 printf("enter name of document to scramble: \n");
@@ -14,14 +14,43 @@ if (ifp == NULL) {
 printf("Sorry, that file does not exist.\n");
 return 0;
 }
-}
+}*/
 
 int main(void) {
-
 FILE *ifp;
+srand (time(NULL));
+int randomNumber = 0;
 char docOpen[50];
+int wordLen;
+char word[30];
+int charTotal = 0;
+int i = 0;
 
-getFileName(docOpen);
+printf("enter name of document to scramble: \n");
+scanf("%s", docOpen);
+
+ifp = fopen(docOpen, "r");
+
+if (ifp == NULL) {
+printf("Sorry, that files does not exist.\n");
+return 0;
+}
+
+while (fscanf(ifp, "%s", word) != EOF) {
+wordLen = strlen(word);
+charTotal = wordLen + charTotal;
+if (wordLen < 3) {
+printf("%s ", word);
+} else {
+for (i = 0; i < wordLen; i++) {
+randomNumber = (rand() % wordLen);
+strcpy(word[i], word[randomNumber]);
+}
+//printf("%s = %d characters \n", word, wordLen);
+}
+}
+printf("the character total is %d\n", charTotal);
+//getFileName(docOpen);
 
 return 0;
 }
