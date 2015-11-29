50163cade2bf83d04262189fe6266ee68332c4c2
Dominick <dominickhera@me.com>
2015-11-29 12:23:52 -0500

    shits finally working, just need to comment and do readme

--- randomWordTest.c
+++ randomWordTest.c
@@ -1,74 +1,101 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
 
+int getFileName (char openFile[], int length);
+int scrambleWord (char word[], int length);
+int processText (char openFile[]);
 
-char getFileName (char openFile[30]);
-char scrambleWord (char word[], int length);
-char processText (char openFile[30]);
-
-int main(void) {
-    char word[30];
-    char scramble[30];
+int main(void)
+{
     char fileName[30];
-    int wordLen = strlen(word);
-    int i, j;
     srand (time(NULL));
-    int randomNumber = 0;
     FILE *ifp;
 
-    getFileName(fileName);
+    getFileName(fileName, 30);
     ifp = fopen(fileName, "r");
 
-    if (ifp == NULL) {
+    if (ifp == NULL) 
+    {
         printf("Sorry, file does not exist\n");
         return 0;
     }
+    fclose(ifp);
 
-    while (fscanf(ifp, "%s", word) != EOF){
-        wordLen = strlen(word);
-        scrambleWord(word, wordLen);
-    }
+    processText(fileName);
 
-    /*    while (fscanf(ifp, "%s", word) != EOF){
-          wordLen = strlen(word);
-          if (wordLen <= 3) {
-          printf("%s ", word);
-          } else {
-          printf("%c", word[0]);
-          for (i = 1; i < (wordLen); i++) {
-          randomNumber = (rand() % wordLen);
-          scramble[i - 1] = word[i];
-          word[i] = scramble[i];
-          printf("%c", word[i]);
-          }
-          printf(" ");
-          }
-          }*/
     printf("\n");
     return 0;
 }
 
-char getFileName (char openFile[30]) {
+int getFileName (char openFile[], int length) 
+{
 
     printf("enter file name to scramble\n");
     scanf("%s", openFile);
 
-    return openFile[30];
+    return 0;
 }
 
-char scambleWord (char word, int length) {
+int scrambleWord (char* word, int length) 
+{
     int i;
-    char letters[length-2];
-    if (length <= 3) {
-        printf("%s ", word);
-    } else {
-       //char letters[length-2];  
-        for (i = 1; i < (length - 2); i++){
-            letters[i] = word[i];
-            printf("%c", letters[i]);
-	  }
-          printf(" ");
-          }
+    if (length <= 3)
+    {
+        return 0;
+    }
+    else
+    {
+        char letters[length-2];  
+        for (i = 1; i < (length - 1); i++)
+        {
+            letters[i-1] = word[i];
+            word[i] = ' ';
+        }
+        for (i = 0; i < 5; i++) 
+        {
+            char temp;
+            int a,b;
+            a = rand() % (length - 2); 	
+            b = rand() % (length - 2);
+            while (a == b) 
+            {
+                b = rand() % (length - 2);
+            }
+            temp = letters[a];
+            letters[a] = letters[b];
+            letters[b] = temp;
+        }
+        for (i = 1; i < (length - 1); i++) 
+        {
+            word[i] = letters[i-1];
+        }
+    }
+    return 0;
+}
+int processText (char fileOpen[])
+{
+    char word[50];
+    int length;
+    int i;
+    FILE *dfp = fopen(fileOpen, "r");
+
+    while (fscanf(dfp, "%s", word) != EOF)
+    {
+        length = strlen(word);
+        for (i = 0; i < length; i++)
+        {
+            if (ispunct(word[i])) 
+            {
+                length = i;
+                break;
+            }
+        }
+        scrambleWord(word, length);
+        printf("%s%c", word, fgetc(dfp));
+    }
+    fclose(dfp);
+    return 0;
 }
