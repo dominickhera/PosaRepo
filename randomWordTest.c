a5a23ecf2e9a86a19aade78abd9c293e0b6b3ffb
Dominick <dominickhera@me.com>
2015-11-29 17:54:27 -0500

    finished commenting on everything

--- randomWordTest.c
+++ randomWordTest.c
@@ -1,29 +1,42 @@
+//dominick hera//
+//cis1500//
+//29/10/15//
+//assignment 2//
+//scrambled words//
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
 
+//initialising functions before defining them after the main function
 int getFileName (char openFile[], int length);
 int scrambleWord (char word[], int length);
 int processText (char openFile[]);
 
 int main(void)
 {
+    //declares variables needed
     char fileName[30];
     srand (time(NULL));
     FILE *ifp;
 
+    //calls function to get name of file and opens it
     getFileName(fileName, 30);
     ifp = fopen(fileName, "r");
-
+    
+    //terminates the program if the file entered doesnt exist
     if (ifp == NULL) 
     {
         printf("Sorry, file does not exist\n");
         return 0;
     }
+	
+    //closes the file to prevent memory leaks
     fclose(ifp);
-
+    
+    //calls process function
     processText(fileName);
 
     printf("\n");
@@ -33,6 +46,7 @@ int main(void)
 int getFileName (char openFile[], int length) 
 {
 
+    //asks for file name and stores it in a string
     printf("enter file name to scramble\n");
     scanf("%s", openFile);
 
@@ -41,19 +55,30 @@ int getFileName (char openFile[], int length)
 
 int scrambleWord (char* word, int length) 
 {
+      
     int i;
+
+    //main scramble function
+    //goes back to main function if the the word is 3 or less letters
     if (length <= 3)
     {
         return 0;
     }
     else
     {
-        char letters[length-2];  
+	//creates a second array to scramble
+	//puts original array with characters through the smaller second array
+        //replaces the original letters in the array with spaces
+	char letters[length-2];  
         for (i = 1; i < (length - 1); i++)
         {
             letters[i-1] = word[i];
             word[i] = ' ';
         }
+
+	//scrambles letters 5 times
+	//declares a temporary variable, as well as a and b to put the letters through
+	//if the randomised numbers between a and b are the same then b gets randomised again
         for (i = 0; i < 5; i++) 
         {
             char temp;
@@ -68,6 +93,9 @@ int scrambleWord (char* word, int length)
             letters[a] = letters[b];
             letters[b] = temp;
         }
+	
+	//puts the second array back into the original one starting at
+	//the second letter in the word
         for (i = 1; i < (length - 1); i++) 
         {
             word[i] = letters[i-1];
@@ -82,6 +110,10 @@ int processText (char fileOpen[])
     int i;
     FILE *dfp = fopen(fileOpen, "r");
 
+    //sends the text file until the end of the file is reached
+    //finds length of each word, and uses that in a for loop
+    //checks for punctuation, if there is punctuation, the length
+    //is set to the current number and movess on to the next word
     while (fscanf(dfp, "%s", word) != EOF)
     {
         length = strlen(word);
@@ -93,9 +125,15 @@ int processText (char fileOpen[])
                 break;
             }
         }
+
+	//calls the scramble function from before  with the first 
+	//word array and length of each word
+	//prints the word in the string and then uses fgetc to grab
+	//the next character in line whether it be a space or new line
         scrambleWord(word, length);
         printf("%s%c", word, fgetc(dfp));
     }
+    //closes the textfile again to prevent memory leaks
     fclose(dfp);
     return 0;
 }
