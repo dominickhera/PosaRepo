dc9e79b125c811959c42ada12400c0302a23b804
Dominick Hera <dominickhera@Dominicks-MacBook-Pro.local>
2016-09-19 15:26:12 -0400

    added lab file

---
+++ cis2520lab2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int isPrime(int num, int divisor)
+{
+	if(num<2)
+		return 1;
+	if(divisor == 1)
+		return 1;
+	else
+	{
+		if(num % divisor == 0)
+			return 0;
+
+		//MORE STUFF THAT I MISSED, I HOPE IT WAS WORTH IT
+	}
+}
+
+/*
+
+
+
+*/
