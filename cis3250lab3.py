65d81dbb37388de3b889f134b26201526d82ed18
Dominick <dominickhera@me.com>
2016-09-27 23:18:42 -0400

    started working on friday lab

---
+++ cis3250lab3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+firstNameList = []
+lastNameList = []
+idList = []
+userOption = 0
+
+
+# userOption = input("choose a number:\n>")
+
+while (userOption != 6):
+	userOption = input("choose a number:\n>")
+	print userOption
