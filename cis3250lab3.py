dedcfc905b5911451b542fb2da089956f232a8fc
Dominick <dominickhera@me.com>
2016-09-27 23:57:04 -0400

    too tired, will finish tomorrow

--- cis3250lab3.py
+++ cis3250lab3.py
@@ -4,6 +4,7 @@ firstNameList = []
 lastNameList = []
 idList = []
 userOption = 0
+listCount = 0
 
 while (userOption != 6):
 	print "(1) Print all students\n(2) Add a student to the list\n(3) Print the information of next student on the list.\n(4) Print the information of previous student on the list.\n(5) Save the entire list of students\n(6) Quit\n"
@@ -15,8 +16,27 @@ while (userOption != 6):
 	if (userOption == 1):
 		print "\nyou selected option 1\n"
 	if (userOption == 2):
-		firstNameEntry = input("enter student first name:\n> ")
+		firstNameEntry = raw_input("enter student first name:\n> ")
 		firstNameList.append(firstNameEntry)
+		print firstNameList
+		lastNameEntry = raw_input("enter student last name:\n> ")
+		lastNameList.append(lastNameEntry)
+		print lastNameList
+		idEntry = raw_input("enter student id number:\n> ")
+		if (isinstance(idEntry, int)):
+			# print(isinstance(idEntry, int))
+			idList.append(idEntry)
+			print idList
+			listCount = listCount + 1
+			print listCount
+		else:
+			print "not a var fam"
+			while ((isinstance(idEntry, int) == 0)):
+				idEntry = raw_input("enter student id number:\n> ")
+			idList.append(idEntry)
+			print idList
+			listCount = listCount + 1
+			print listCount
 	if (userOption == 3):
 		print "\nyou selected option 3\n"
 	if (userOption == 4):
