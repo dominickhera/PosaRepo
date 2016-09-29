b4a4da4ab0975d4837762cbabb25f50cf6f1ee4e
Dominick Hera <dominickhera@Dominicks-MacBook-Pro.local>
2016-09-29 19:23:48 -0400

    more work on lab

--- cis3250lab3.py
+++ cis3250lab3.py
@@ -3,8 +3,10 @@
 firstNameList = []
 lastNameList = []
 idList = []
+theFile = open('student.txt', 'w')
 userOption = 0
 listCount = 0
+viewCount = 0
 i = 0
 
 while (userOption != 6):
@@ -15,12 +17,13 @@ while (userOption != 6):
 		print "\nplease enter a number between 1 - 6\n"
 
 	if (userOption == 1):
+		i = 0
 		print "\nyou selected option 1\n"
 		for x in firstNameList:
 			print "%s %s - %s" % (firstNameList[i], lastNameList[i], idList[i])
 			i = i + 1
 		print "\n"
-			
+
 	if (userOption == 2):
 		firstNameEntry = raw_input("enter student first name:\n> ")
 		firstNameList.append(firstNameEntry)
@@ -44,10 +47,24 @@ while (userOption != 6):
 		# 	listCount = listCount + 1
 		# 	print listCount
 	if (userOption == 3):
-		print "\nyou selected option 3\n"
+		# print "\nyou selected option 3\n"
+		if (viewCount + 1 > listCount):
+			viewCount = 0
+			print "%s %s - %s\n" % (firstNameList[viewCount], lastNameList[viewCount], idList[viewCount])
+		else:
+			viewCount = listCount + 1
+			print "%s %s - %s\n" % (firstNameList[viewCount], lastNameList[viewCount], idList[viewCount])
 	if (userOption == 4):
-		print "\nyou selected option 4\n"
+		# print "\nyou selected option 4\n"
+		viewCount = (listCount - 1)
+		print "%s %s - %s\n" % (firstNameList[viewCount], lastNameList[viewCount], idList[viewCount])
 	if (userOption == 5):
-		print "\nyou selected option 5\n"
+		i = 0
+		# print "\nyou selected option 5\n"
+		theFile.write("First Name  Last Name  - Student ID\n")
+		for x in firstNameList:
+			theFile.write("%s %s - %s" % (firstNameList[i], lastNameList[i], idList[i]))
+			i = i + 1
+
 	if (userOption == 6):
 		print "\ngoodbye\n"
