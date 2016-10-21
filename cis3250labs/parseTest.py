8d8c99088b8258a8f3a45f7df377fc9f59697ff0
Dominick <dominickhera@me.com>
2016-10-21 01:59:45 -0400

    parsing almost done, trying to figure out how to make it invalidate if an equation is like 12+ or something

--- cis3250labs/parseTest.py
+++ cis3250labs/parseTest.py
@@ -3,16 +3,42 @@ import re
 
 userInput = raw_input("input equation\n")
 
-i = -1
-numCount = 0;
-operandCount = 0;
-charCount = 0;
+# i = 0
+numCount = 0
+operandCount = 0
+entryBracketCount = 0
+exitBracketCount = 0
+charCount = 0
 
+print (len(userInput))
 for i in range(len(userInput)):
-	if userInput[i] == '1':
-		print userInput[i] , " is int "
-	elif (userInput[i] == '+') or (userInput[i] == '-') or (userInput[i] == '*') or (userInput[i] == '/'):
-		print userInput[i] , " is an operand"
+	if (re.search('[a-zA-Z]+', userInput[i])):
+		charCount = charCount + 1
+	elif (re.search('[0-9]+', userInput[i])):
+		# print userInput[i]
+		numCount = numCount + 1
+	elif (re.search('[[\+|\-|\*|\/]+', userInput[i])):
 		operandCount = operandCount + 1
-		print operandCount , " operands found so far"
-	
+		# if userInput[i + 1] == len(userInput):
+		# if len(userInput) == userInput[i]:
+		# 	print "invalid expression"
+		# 	exit(0)
+		# else:
+			# if(re.search('[^a-zA-Z0-9]+', userInput[i + 1])):
+				# print 'next char is not a number'
+	elif (re.search('[(]+', userInput[i])):
+		entryBracketCount = entryBracketCount + 1
+	elif (re.search('[)]+', userInput[i])):
+		exitBracketCount = exitBracketCount + 1
+
+if (entryBracketCount != exitBracketCount):
+	print "invalid expression"
+	exit(0)
+elif operandCount == 0:
+	print "invalid expression"
+	exit(0)
+elif ((numCount == 0) and (charCount == 0)):
+	print "invalid expression"
+	exit(0)
+else:
+	print "valid expression"
