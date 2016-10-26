6df108fc4df5273b6bef8ba27d264eaeaf2028f5
Dominick <dominickhera@me.com>
2016-10-26 02:12:12 -0400

    no work besides creating lab6, maybe baclaurin series will make more sense when it's not 2 am

---
+++ cis3250labs/cis3250lab6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from __future__ import division
+from Tkinter import *
+
+import re
+import math
+import tkMessageBox
+
+xVar = input("input x variable: ")
+print xVar
+deltaE = input("input your delta E variable: ")
+print deltaE
