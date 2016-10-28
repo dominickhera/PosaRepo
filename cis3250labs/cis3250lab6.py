675c259e287f65251400aa1333a7ba066034091b
Dominick Hera <dominickhera@me.com>
2016-10-28 16:10:12 -0400

    fuck school computers man

--- cis3250labs/cis3250lab6.py
+++ cis3250labs/cis3250lab6.py
@@ -6,28 +6,35 @@ from Tkinter import *
 import math
 import tkMessageBox
 
-xVar = input("input x variable: ")
-deltaE = input("input your delta E variable (predefined error value): ")
+def calculate():
+	xVar = input("input x variable: ")
+	deltaE = input("input your delta E variable (predefined error value): ")
 
 
 
-newX = xVar * (math.pi/180)
-c = math.sin(newX)
+	newX = xVar * (math.pi/180)
+	c = math.sin(newX)
 
 
-y = 0
-k = 1
-n = 0
+	y = 0
+	k = 1
+	n = 0
 
-while ((math.fabs(c - y)) > deltaE):
-	if (y % 2 == 0):
-		y = y + (newX**k)/(math.factorial(k))
-		print y
-		print "hey"
-	else:
-		y = y - (newX**k)/(math.factorial(k))
-		print y
-		print "hoe"
-	n = n + 1
-	print n, "cycles"
-	k = k + 2
+	while ((math.fabs(c - y)) > deltaE):
+		if (y % 2 == 0):
+			y = y + (newX**k)/(math.factorial(k))
+		else:
+			y = y - (newX**k)/(math.factorial(k))
+		n = n + 1
+		k = k + 2
+
+
+root = Tk()
+
+entryBox1 = Entry(root)
+entryBox1.pack()
+
+bDisplayMessage = Button(root, text ="calculate", command = calculate)
+bDisplayMessage.pack()
+
+root.mainloop()
