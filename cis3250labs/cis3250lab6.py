9c37a12d638d1be0b32b2d9bcb6fb0a9fc3af421
Dominick Hera <dominickhera@me.com>
2016-10-28 16:01:10 -0400

    laptop about to die, might need to transition to school computer

--- cis3250labs/cis3250lab6.py
+++ cis3250labs/cis3250lab6.py
@@ -3,11 +3,35 @@
 from __future__ import division
 from Tkinter import *
 
-import re
 import math
 import tkMessageBox
 
 xVar = input("input x variable: ")
-print xVar
-deltaE = input("input your delta E variable: ")
-print deltaE
+print xVar , "is your x var"
+deltaE = input("input your delta E variable (predefined error value): ")
+print deltaE, "is your delta e"
+
+
+newX = xVar * (math.pi/180)
+c = math.sin(newX)
+print c , "is your c"
+
+y = 0
+k = 1
+n = 0
+
+while ((math.fabs(c - y)) > deltaE):
+	if (y % 2 == 0):
+		y = y + (newX**k)/(math.factorial(k))
+		print y
+		print "hey"
+	else:
+		y = y - (newX**k)/(math.factorial(k))
+		print y
+		print "hoe"
+	n = n + 1
+	print n, "cycles"
+	k = k + 2
+
+print y , "is your final y value"
+print "cycled ", n , " total times"
