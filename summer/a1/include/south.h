ce02166089bac8fe7518dced5a6e65c5778bbf3e
Dominick Hera <dominickhera@me.com>
2017-06-02 12:26:29 -0400

    added .h files for each direction

--- summer/a1/include/south.h
+++ summer/a1/include/south.h
@@ -15,22 +15,22 @@
 *Node of a linked list. This list is doubly linked, meaning that it has points to both the node immediately in front 
 of it, as well as the node immediately behind it.
 **/
-typedef struct listNode{
+typedef struct listSouthNode{
     void *data;
-    struct listNode *previous;
-    struct listNode *next;
-} Node;
+    struct listSouthNode *previous;
+    struct listSouthNode *next;
+} SouthNode;
 
 /**
  *Dummy head of the list. Contains no actual data on it beyond a pointer to the front and end of the list.
  **/
-typedef struct listHead{
-    Node *head;
-    Node *tail;
+typedef struct listSouthHead{
+    Node *southHead;
+    Node *southTail;
     void (*deleteData)(void *toBeDeleted);
     int (*compare)(const void *first,const void *second);
     void (*printData)(void *toBePrinted);
-} List;
+} SouthList;
 
 /**Function to point the list head to the appropriate functions. Allocates memory to the struct.
 *@return pointer to the list head
@@ -38,7 +38,7 @@ typedef struct listHead{
 *@param deleteFunction function pointer to delete a single piece of data from the list
 *@param compareFunction function pointer to compare two nodes of the list in order to test for equality or order
 **/
-List *initializeList(void (*printFunction)(void *toBePrinted),void (*deleteFunction)(void *toBeDeleted),int (*compareFunction)(const void *first,const void *second));
+SouthList *initializeList(void (*printFunction)(void *toBePrinted),void (*deleteFunction)(void *toBeDeleted),int (*compareFunction)(const void *first,const void *second));
 
 /**Function for creating a node for a linked list. This node contains generic data and may be connected to 
 other notes in a list.
@@ -48,27 +48,27 @@ other notes in a list.
 *@return On success returns a node that can be added to a linked list. On failure, returns NULL.
 *@param data - is a generic pointer to any data type.
 **/
-Node *initializeNode(void *data);
+SouthNode *initializeSouthNode(void *data);
 
 /**Inserts a Node to the front of a linked list. The list then updates accordingly to adhere to the ADT.
 *@pre 'List' type must exist and be used in order to keep track of the linked list.
 *@param list pointer to the dummy head of the list
 *@param toBeAdded a pointer to data that is to be added to the linked list
 **/
-void insertFront(List *list, void *toBeAdded);
+void insertSouthFront(SouthList *southlist, void *toBeAdded);
 
 /**Inserts a Node to the back of a linked list. The list then updates accordingly to adhere to the ADT.
 *@pre 'List' type must exist and be used in order to keep track of the linked list.
 *@param list pointer to the dummy head of the list
 *@param toBeAdded a pointer to data that is to be added to the linked list
 **/
-void insertBack(List *list, void *toBeAdded);
+void insertSouthBack(SouthList *southlist, void *toBeAdded);
 
 /** Deletes the entire linked list head to tail, starting with the nodes, followed by the list itself.
 *@pre 'List' type must exist and be used in order to keep track of the linked list.
 *@param list pointer to the List-type dummy node
 **/
-void deleteList(List *list);
+void deleteSouthList(SouthList *southlist);
 
 /** Uses the comparison function in the List struct to place the element in the appropriate position in the list.
 *this is intended to be used from the beginning in order to keep the list completely sorted.
@@ -78,7 +78,7 @@ void deleteList(List *list);
 as a pointer to the first and last element of the list.
 *@param toBeAdded a pointer to data that is to be added to the linked list
 **/
-void insertSorted(List *list, void *toBeAdded);
+void insertSouthSorted(SouthList *southlist, void *toBeAdded);
 
 /**Function to remove a node from the list and alter the pointers accordingly to not disrupt the order of the 
  *data structure. 
@@ -88,40 +88,33 @@ void insertSorted(List *list, void *toBeAdded);
  *@param toBeDeleted pointer to data that is to be removed from the list
  *@return returns EXIT_SUCCESS on success, and EXIT_FAILURE when empty. Returns -1 when the node cannot be found.
  **/
-int deleteDataFromList(List *list, void *toBeDeleted);
+int deleteDataFromSouthList(SouthList *southlist, void *toBeDeleted);
 
 /**Function to return the data at the front of the list.
  *@pre The list exists and has memory allocated to it
  *@param list pointer to the dummy head of the list containing the head of the list
  *@return pointer to the data located at the head of the list
  **/
-void *getFromFront(List *list);
+void *getFromSouthFront(SouthList *southlist);
 
 /**Function to return the data at the back of the list.
  *@pre The list exists and has memory allocated to it
  *@param list pointer to the dummy head of the list containing the tail of the list
  *@return pointer to the data located at the tail of the list
  **/
-void *getFromBack(List *list);
+void *getFromSouthBack(SouthList *southlist);
 
 /**Function to print list from head to tail. This will utilize the list's printData function pointer to print.
  *@pre List must exist, but does not have to have elements.
  *@param list Pointer to linked list dummy head.
  **/
-void printForward(List *list);
+void printSouthForward(SouthList *southlist);
 
 /**Function to print list from tail to head. This will utilize the list's printData function pointer to print.
  *@pre List must exist, but does not have to have elements.
  *@param list Pointer to linked list dummy head.
  **/
-void printBackwards(List *list);
-
-
-
-
-
-
-
+void printSouthBackwards(SouthList *southlist);
 
 
 #endif
--- summer/a1/src/main.c
+++ summer/a1/src/main.c
@@ -1,14 +1,72 @@
-#include "LinkedListAPI.h"
-#include "parse.h"
+// #include "north.h"
+// #include "east.h"
+// #include "south.h"
+//
+// #include "parse.h"
+#include <stdio.h>
+#include <string.h>
 
-void printFunction(void * data)
-{
-	printf("%s", (char *)data);
-}
+// void printFunction(void * data)
+// {
+// 	printf("%s", (char *)data);
+// }
+
+// int compareFunction(const void *first,const void *second)
+// {
+// 	int tempVal = 0;
+
+// 	if(*(int *)first > *(int *)second || *(int *)first == *(int *)second)
+// 	{
+// 		return first;
+// 	}
+// 	else
+// 	{
+// 		tempVal = first;
+// 		first = second;
+// 		second = tempVal;
+// 		return first;
+// 	}
+// }
 
 int main(int argc, char ** argv)
 {
-	printf("hi there\n");
+    char line[256];
+    char * parse;
+    int clk = 1;
+    FILE * fp;
 
-	return 0;
+    fp = fopen(argv[1], "r");
+
+    if(fp == NULL)
+    {
+        printf("could not find file\n");
+        return 0;
+    }
+
+    if(argc != 2)
+    {
+        printf("incorrect amount of input\n");
+    }
+
+    while(fgets(line, sizeof(line), fp) != NULL)
+    {
+        parse = strtok (line," ");
+        while (parse != NULL)
+        {
+            printf ("%s\n",parse);
+            parse = strtok (NULL, " ");
+        }
+        // printf("%s", line); 
+    }  
+
+    // pch = strtok (str," ");
+    // while (pch != NULL)
+    //  {
+    //    printf ("%s\n",pch);
+    //    pch = strtok (NULL, " ");
+    //  }
+
+    printf("\n");  
+
+    return 0;
 }
