04ea46751bbd5fa48f73dfaa6769cefaecbb6ffc
dominickhera <dominickhera@me.com>
2017-06-10 23:15:30 -0400

    most functions finished, need to add and lookup and then trouble shoot lol

--- summer/lab2/src/HashTableAPI.c
+++ summer/lab2/src/HashTableAPI.c
@@ -3,25 +3,95 @@
 HTable *createTable(size_t size, int (*hashFunction)(size_t tableSize, int key),void (*destroyData)(void *data),void (*printData)(void *toBePrinted))
 {
 
+	HTable * temp = NULL;
+
+	temp = malloc(sizeof(HTable));
+
+	if(temp == NULL)
+	{
+		return NULL;
+	}
+
+	for(int i = 0; i < size; i++)
+	{
+		temp->table[i] = NULL;
+	}
+
+	temp->deleteData = destroyData;
+	temp->hashFunc = hashFunction
+	temp->printFunction = printData;
+
+	return temp;
+
 }
 
 Node *createNode(int key, void *data)
 {
+
+	Node * temp;
+
+	if(temp == NULL)
+	{
+		return NULL;
+	}
+
+	temp->next = NULL;
+
+	return temp;
+
 
 }
 
 void destroyTable(HTable *hashTable)
 {
 
+	HTable * temp1;
+	HTable * temp2;
+
+	for(int i = 0; i < hashTable->size; i++)
+	{
+
+		temp1 = hashTable->data[i];
+		while(temp1)
+		{
+			temp2 = temp1;
+			temp1 = temp1->next;
+			free(temp2->key);
+			free(temp2);
+		}
+
+	}
+
+	free(hashTable);
+
 }
 
 void insertData(HTable *hashTable, int key, void *data)
 {
 
+
+
 }
 
 void removeData(HTable *hashTable, int key)
 {
+
+	HTable * temp;
+
+	if(*hashTable == NULL)
+	{
+		removeData(&(*hashTable)->next, key);
+	}
+	else
+	{
+		if(*hashTable->key == key)
+		{
+			temp = *hashTable;
+			*hashTable = (*hashTable)->next;
+			free(temp->key);
+			free(temp);
+		}
+	}
 
 }
 
