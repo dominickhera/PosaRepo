4949aab7b22ed452995ef47992e86da7c5783b89
dominickhera <dominickhera@me.com>
2017-06-18 14:16:25 -0400

    hash doesnt give me errors, lets go test it with the self testing thing

--- summer/lab2/src/HashTableAPI.c
+++ summer/lab2/src/HashTableAPI.c
@@ -3,43 +3,43 @@
 HTable *createTable(size_t size, int (*hashFunction)(size_t tableSize, int key),void (*destroyData)(void *data),void (*printData)(void *toBePrinted))
 {
 
-	HTable * temp = NULL;
+    HTable * temp = NULL;
 
-	temp = malloc(sizeof(HTable));
+    temp = malloc(sizeof(HTable));
 
-	if(temp == NULL)
-	{
-		return NULL;
-	}
+    if(temp == NULL)
+    {
+        return NULL;
+    }
 
-	for(int i = 0; i < size; i++)
-	{
-		temp->table[i] = NULL;
-	}
+    for(int i = 0; i < size; i++)
+    {
+        temp->table[i] = NULL;
+    }
 
-	temp->destroyData = destroyData;
-	temp->hashFunction = hashFunction;
-	temp->printData = printData;
+    temp->destroyData = destroyData;
+    temp->hashFunction = hashFunction;
+    temp->printData = printData;
 
-	return temp;
+    return temp;
 
 }
 
 Node *createNode(int key, void *data)
 {
 
-	Node * temp = malloc(sizeof(Node));
+    Node * temp = malloc(sizeof(Node));
 
-	if(temp == NULL)
-	{
-		return NULL;
-	}
+    if(temp == NULL)
+    {
+        return NULL;
+    }
 
-	temp->key = key;
-	temp->data = data;
-	temp->next = NULL;
+    temp->key = key;
+    temp->data = data;
+    temp->next = NULL;
 
-	return temp;
+    return temp;
 
 
 }
@@ -47,79 +47,95 @@ Node *createNode(int key, void *data)
 void destroyTable(HTable *hashTable)
 {
 
-	Node * temp = NULL;
-	HTable * temp2;
-	// HTable * temp2;
+    Node * temp = NULL;
+    // HTable * temp2;
+    // // HTable * temp2;
 
-	for(int i = 0; i < hashTable->size; i++)
-	{
+    while(temp != NULL)
+    {
+    	removeData(hashTable, temp->key);
+    	temp = temp->next;
+    }
 
-		removeData(temp2, temp->key);
+    // for(int i = 0; i < hashTable->size; i++)
+    // {
 
-		// temp1 = Node->data;
-		while(temp != NULL)
-		{
-			removeData(temp2, temp->key);
-			 temp2 = temp->next;
-			// temp1 = temp1->next;
-			// free(temp2->key);
-			// free(temp2);
-		}
+    //     Node * temp = removeData(hashTable, temp->key);
 
-	}
+    //     // temp1 = Node->data;
+    //     while(temp != NULL)
+    //     {
+    //         temp = removeData(temp, temp->key);
+    //         // temp = temp->next;
+    //         // temp1 = temp1->next;
+    //         // free(temp2->key);
+    //         // free(temp2);
+    //     }
 
-	free(hashTable);
+    // }
+
+    free(hashTable);
 
 }
 
 void insertData(HTable *hashTable, int key, void *data)
 {
 
-	int count;
+    int count;
 
-	Node * temp;
+    Node * temp;
 
-	count = hashTable->hashFunction(hashTable->size, key);
+    count = hashTable->hashFunction(hashTable->size, key);
 
-	temp = createNode(key, data);
+    temp = createNode(key, data);
 
-	temp->next = hashTable->table[count];
-	hashTable->table[count] = temp;
-
-
-
-
+    temp->next = hashTable->table[count];
+    hashTable->table[count] = temp;
 
 }
 
 void removeData(HTable *hashTable, int key)
 {
 
-	Node * temp;
-	// temp = Node;
+    int tempKey = 0;
+    tempKey = hashTable->hashFunction(hashTable->size, key);
+    Node * temp = hashTable->table[tempKey];
 
-	// HTable * tempTable;
-	// tempTable = HTable;
+    while(temp != NULL)
+    {
 
-	// if(*hashTable == NULL)
-	// {
-	// 	return null;
-	// }
+        if(temp->key != tempKey)
+        {
+            temp = temp->next;
+        }
+        else
+        {
+            free(temp->data);
+            free(temp);
+            temp = temp->next;
+        }
 
-
-	if(temp->key == key)
-	{
-			temp = *hashTable;
-			*hashTable = Node->next;
-			free(temp->data);
-			free(temp);
-	}
-	
+    }
 
 }
 
 void *lookupData(HTable *hashTable, int key)
 {
+
+	int tempKey = 0;
+	tempKey = hashTable->hashFunction(hashTable->size, key);
+
+	Node * temp = hashTable->table[tempKey];
+
+	if(temp->key == tempKey)
+	{
+		return temp->data;
+	}
+	else
+	{
+		return NULL;
+	}
+
 
 }
 
