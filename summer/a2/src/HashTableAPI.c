155ce6912684dc94ec81054659b0a09b8d909f98
dominickhera <dominickhera@me.com>
2017-06-22 01:49:09 -0400

    attempting to make somethign work

--- summer/a2/src/HashTableAPI.c
+++ summer/a2/src/HashTableAPI.c
@@ -29,17 +29,21 @@ HTable *createTable(size_t size, int (*hashFunction)(size_t tableSize, char * ke
 
 Node *createNode(char * key, void *data)
 {
-
+printf("p1\n");
     Node * temp = malloc(sizeof(Node));
-
+printf("p2\n");
     if(temp == NULL)
     {
         return NULL;
     }
 
+printf("p3\n");
     temp->key = key;
+    printf("p4\n");
     temp->data = data;
+    printf("p5\n");
     temp->next = NULL;
+printf("p6\n");
 
     return temp;
 }
@@ -65,20 +69,20 @@ void insertData(HTable *hashTable, char * key, void *data)
     if(hashTable != NULL)
     {
         // printf("data before insert: %s\n", (char *) temp->data);
-        printf("fb 1, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
+        // printf("fb 1, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
         int count = 0;
-         printf("fb 2, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
+         // printf("fb 2, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
         count = hashTable->hashFunction(hashTable->size, key);
-         printf("fb 3, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
+         // printf("fb 3, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
         Node * temp = createNode(key, data);
-         printf("fb 4, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
+         // printf("fb 4, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
         // printf("data after insert: %s\n", (char *) temp->data);
         temp->next = hashTable->table[count];
-         printf("fb 5, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
+         // printf("fb 5, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
         hashTable->table[count] = temp;
-         printf("fb 6, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
+         // printf("fb 6, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
         printf("insertKey: %d\n", count);
-         printf("fb 7, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
+         // printf("fb 7, data[%s]\n", (char*)lookupData(hashTable, "facebook"));
     
     printf("data after insert: %s\n", (char *) temp->data);
 }
@@ -125,7 +129,7 @@ void *lookupData(HTable *hashTable, char * key)
     // printf("2\n");
     tempKey = hashTable->hashFunction(hashTable->size, key);
     // printf("3\n");
-    // printf("tempkey: %d\n", tempKey);
+    printf("tempkey69: %d\n", tempKey);
     Node * temp = hashTable->table[tempKey];
     // printf("4\n");
     if(temp == NULL || key == NULL || temp->data == NULL)
@@ -135,19 +139,20 @@ void *lookupData(HTable *hashTable, char * key)
     }
 
     // printf("6\n");
-    // while(temp != NULL)
-    // {
+    while(temp != NULL)
+    {
         // printf("data: %s\n", (char *)temp->data);
         // printf("7\n");
         if(strcmp(temp->key, key) == 0)
         {
-            // printf("8\n");
+            printf("8\n");
             return temp->data;
+            break;
         }
-        // temp = temp->next;
+        temp = temp->next;
         // printf("9\n");
 
-    // }
+    }
     // printf("10\n");
     return NULL;
 }
