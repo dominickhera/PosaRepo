23138027b4cc8ff1e1b8080b2a0af4fd368ed7b4
dominickhera <dominickhera@me.com>
2017-07-07 00:38:52 -0400

    trying to fix insert heap, currently it doesnt insert properly and just seg faults

--- summer/lab3/src/heap.c
+++ summer/lab3/src/heap.c
@@ -4,14 +4,13 @@ Heap *createHeap(size_t initialSize, void *htype, void (*destroyDataFP)(void *da
 {
 
     Heap * temp = malloc(sizeof(Heap) * initialSize);
-
     if(temp == NULL)
     {
         return NULL;
     }
-
     // temp->HEAP_TYPE = htype;
-    strcpy(temp->type, htype);
+    // strcpy(temp->type, htype);
+    temp->type = htype;
     temp->initialSize = initialSize;
     temp->destroyDataFP = destroyDataFP;
     temp->printNodeFP = printNodeFP;
@@ -28,19 +27,26 @@ Node *createHeapNode(void *data)
     //     set the node's data to the parameter data value
     //     return the pointer to the new Node
 
+	printf("1\n");
     Node * temp = malloc(sizeof(Node));
+    printf("2\n");
     temp->data = malloc(sizeof(data));
-
+    printf("3\n");
     if(temp == NULL)
     {
         return NULL;
     }
-
+    printf("4\n");
     temp->left = NULL;
+    printf("5\n");
     temp->right = NULL;
+    printf("6\n");
     temp->parent = NULL;
+    printf("7\n");
+    // temp->data = data;
+    // printf("8\n");
     strcpy(temp->data, data);
-
+    printf("8\n");
     return temp;
 }
 
@@ -50,19 +56,24 @@ void insertHeapNode(Heap *heap, void *data)
     //    locate the next position in the heap (the left most position with no node)
     //    add the new node to the located position
     //    reheapify the heap to maintain the heap property
-
-    heap->initialSize++;
-    // int tempSize = heap->initialSize;
+	printf("9\n");
+    // heap->initialSize++;
+    printf("10\n");
+    int tempSize = heap->initialSize++;
     Node * temp = createHeapNode(data);
-    heap->heapTable[heap->initialSize] = temp;
+    printf("11\n");
+    // heap->heapTable[tempSize	] = temp;
+    printf("12\n");
 
 
     if(strcmp((char *)heap->type, "MAX_HEAP") == 0)
     {
+    	printf("13\n");
         reheapifyMax(heap, temp);
     }
     else
     {
+    	printf("14\n");
         reheapifyMin(heap, temp);
     }
 }
@@ -94,7 +105,7 @@ void changeHeapType(Heap *heap)
     if(strcmp((char *)heap->type, "MAX_HEAP") == 0)
     {
         strcpy(heap->type, "MIN_HEAP");
-        reheapifyMin(heap, heap);
+        reheapifyMin(heap, heap->heapTable[0]);
     }
     else
     {
@@ -119,20 +130,25 @@ void reheapifyMax(Heap * heap, Node * newNode)
     //    swap positions of newNode and Parent Node
     //    parentNode = get parent node of newNode (has changed because of the swap)
     // }
-
+	printf("15\n");
     Node * swapVar = newNode;
+    printf("16\n");
     Node * tempParent = newNode->parent;
-
+    printf("17\n");
 
     while(newNode->data > tempParent->data)
     {
-
+    	printf("18\n");
         swapVar = newNode;
+        printf("19\n");
         newNode = tempParent;
+        printf("20\n");
         tempParent = swapVar;
+        printf("21\n");
         tempParent = newNode->parent;
-
+        printf("22\n");
     }
+    printf("23\n");
 }
 
 void reheapifyMin(Heap * heap, Node * newNode)
--- summer/lab3/src/main.c
+++ summer/lab3/src/main.c
@@ -6,6 +6,17 @@
 int main()
 {
 
-	printf("hi\n");
+	char * data[] = {"12", "23", "41", "55", "11", "42", "41", "99"};
+
+	Heap * heapThing = createHeap(500, "MAX_HEAP", &free, &printData, &compareFunction);
+
+	for(int i = 0; i < 8; i++)
+	{
+		insertHeapNode(heapThing, data[i]);
+	}
+
+	
+
+	return 0;
 
 }
