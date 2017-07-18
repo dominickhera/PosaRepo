faff0bcad1a08e672c188acd393dd362cf65a6c7
Dominick Hera <dominickhera@me.com>
2017-07-18 02:35:54 -0400

    attempting to add the UI, new data file, added more to README

--- summer/a3/include/functions.h
+++ summer/a3/include/functions.h
@@ -4,9 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ncurses.h>
 #include "heap.h"
 
 void printData(void * data);
 int compareFunction(const void *first,const void *second);
+void passwordChecker(char * masterPassword, char * passwordCheck, int maxX, int maxY);
+int passwordCheckerMainMenu(char * masterPassword, char * passwordCheck, int maxX, int maxY, char * menuOptions[]);
+void clearTextLine(int x, int y);
+void makeMainMenu(int maxX, int maxY);
+void makeMainMenuOptions(int maxX, int maxY, int i, char * menuOptions);
+void clearMainMenu(int maxX, int maxY);
+void getPos(int *, int *);
+
 
 #endif
--- summer/a3/src/main.c
+++ summer/a3/src/main.c
@@ -1,6 +1,10 @@
 #include "heap.h"
 #include "functions.h"
 
+
+#define character "X"
+#define wall '=';
+
 int main(int argc, char* argv[])
 {
 
@@ -13,9 +17,18 @@ int main(int argc, char* argv[])
     int parseCount = 0;
     int cycle = 0;
     int cycleCheck = 0;
+    int c;
+    int maxX;
+    int maxY;
+    int y = 0;
+    int x = 0;
+    int prevX;
+    int prevY;
     FILE *fp;
     FILE *fo;
 
+    char * menuOptions[] = {"Add New Client", "Remove Client", "Cycle through Clients", "Print List", "Exit"}; 
+    int tempMenuNumb[7];
     if(argc != 2)
     {
         printf("wrong amount of arguements entered\n");
@@ -83,105 +96,363 @@ int main(int argc, char* argv[])
 
     // printHeap(heap);
 
-    while(choice != 6)
+    initscr();
+    // noecho();
+    getmaxyx(stdscr,maxY,maxX);
+
+    makeMainMenu(maxX, maxY);
+
+    // passwordChecker(masterPassword, passwordCheck, maxX, maxY);
+
+    for(int i = 1; i < 6; i++)
+    {
+        makeMainMenuOptions(maxX, maxY, i, menuOptions[i - 1]);
+
+    }
+
+    mvprintw((maxY/8),5,character);
+    move((maxY/8),5);
+
+
+    c = getch();
+
+     while (c != 'q')
     {
 
-        printf("\n1.add new client\n2.remove old client\n3.cycle through heap\n4.print list\n5.save\n6.exit\n\n/> ");
-    	scanf("%d", &choice);
-    	switch(choice)
-    	{
-    		case 1:
-                memset(tempID, 0, strlen(line));
-                memset(tempPriority, 0, strlen(line));
-                memset(tempSymptomCode, 0, strlen(line));
-    			printf("enter client ID: ");
-                scanf("%s", tempID);
-                printf("enter Priority (1-5): ");
-                scanf("%s", tempPriority);
-                printf("enter symptom code: ");
-                scanf("%s", tempSymptomCode);
-                insertHeapNode(heap, atoi(tempPriority), tempID, tempSymptomCode);
-                // printf("heapID: %d, ID: %s, Priority: ")
-    			break;
-    		case 2:
-                memset(tempID, 0, strlen(line));
-    			// printf("remove old client\n");
-                printf("enter client ID: ");
-                scanf("%s", tempID);
-                deleteHeapNode(heap, tempID);
-    			break;
-    		case 3:
-    			// printf("go through heap from top to bottom\n");
-                // if(cycle >= heap->initialSize)
-                // {
-                    cycleCheck = 0;
-                // }
-                while(cycleCheck != 3)
+        if(c == 'w') 
+        {
+            getPos(&y,&x);
+            if(y > 1) 
+            {
+                if((y - (maxY/8)) > 1)
                 {
-                    printf("<(1):(2)> Exit(3)\n\n/> ");
-                    scanf("%d", &cycleCheck);
-                switch(cycleCheck)
-                {
-                    // printf("<(1):(2)> Exit(3)\n\n");
-                    // scanf("%d", &cycleCheck);
-                    case 1:
-                        cycle--;
-                        if(cycle < 0)
-                        {
-                            cycle = heap->initialSize - 1;
-                        }
-                        // printf("cycle: %d\n", cycle);
-                        printf("[%d/%zu]: Client ID: %s, Priority: %d, Symptom Code: %s\n", cycle + 1, heap->initialSize, heap->heapTable[cycle]->clientID, heap->heapTable[cycle]->priority, heap->heapTable[cycle]->symptomCode);
-                        // cycle--;
-                        break;
-                    case 2:
-                        cycle++;
-                        if(cycle > heap->initialSize - 1)
-                        {
-                            cycle = 0;
-                        }
-                        // printf("cycle: %d\n", cycle);
-                        printf("[%d/%zu]: Client ID: %s, Priority: %d, Symptom Code: %s\n", cycle + 1, heap->initialSize, heap->heapTable[cycle]->clientID, heap->heapTable[cycle]->priority, heap->heapTable[cycle]->symptomCode);
-                        // cycle++;
-                        break;
-                    case 3:
-                        break;
-                    default:
-                        break;
+
+                    printw(" "); 
+                    mvprintw((y - (maxY/8)), (x),character);
+                    move(y - (maxY/8) , x);
                 }
             }
-                // printf("[%d/%zu]: Client ID: %s, Priority: %d, Symptom Code: %s\n", cycle + 1, heap->initialSize, heap->heapTable[cycle]->clientID, heap->heapTable[cycle]->priority, heap->heapTable[cycle]->symptomCode);
-    			// cycle++;
-                break;
-    		case 4:
-    			// printf("printing list\n");
-                printHeap(heap, fo, 2);
-    			break;
-    		case 5:
-                // char tempMaxorMin[256];
-    			printf("heaipfy\n");
-                reheapifyMin(heap, 0);
-                // Node * tempMaxorMin = getMaxorMin(heap);
-                // printf("temp: %s\n", tempMaxorMin->clientID);
-                // getMaxorMin(heap);
 
-    			break;
-    		case 6:
+        }
+        else if(c == 's')
+        {
+            getPos(&y,&x);
+            if(y < (maxY - (maxY/8)))
+            {
+                if((y + (maxY/8)) < ((maxY/3) * 2))
+                {
+                    printw(" ");
+                    mvprintw((y+(maxY/8)), x,character);
+                    move((y + (maxY/8)), x);
+                }
+            }
+        }
+        else if(c == '\n')
+        {
+
+            getPos(&y, &x);
+
+            prevY = y;
+            prevX = x;
+            for(int i = 1; i < 7; i++)
+            {
+                tempMenuNumb[i] = (maxY/8) * i;
+            }
+
+            for(int i = 0; i < maxX; i++)
+            {
+                mvaddch(((maxY/3)*2) + 3, i, ' ');
+            }
+
+            if(y == tempMenuNumb[1])
+            {
+                echo();
+                clearTextLine((((maxY/3)*2) + 2),0);
+                memset(tempPass, 0, 256);
+                memset(tempKey, 0, 256);
+                clearMainMenu(maxX, maxY);
+                // if((passwordCheckerMainMenu(masterPassword, passwordCheck, maxX, maxY, menuOptions)) == 1)
+                // {
+                    echo();
+                    mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 5), "%s", "enter clientID : ");
+                    getstr(tempID);
+                    // if(lookupData(hashTable, tempKey) == NULL)
+                    // {
+                        echo();
+                        clearTextLine(((maxY/6)*2) , (((maxX/6) * 2) - 5));
+                        mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 5), "%s", "enter new client priority : ");
+                        getstr(tempPriority);
+                        clearTextLine(((maxY/6)*2) , (((maxX/6) * 2) - 5));
+                        mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 5), "%s", "enter symptom Code: ");
+                        getstr(tempSymptomCode);
+                        // insertData(hashTable, tempKey, tempPass);
+                        clearTextLine(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5));
+                        insertHeapNode(heap, atoi(tempPriority), tempID, tempSymptomCode);
+                        mvprintw(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5), "%s", "new client successfully entered.");
+
+                        
+                        // passwordVaultSize++;
+                    // }
+                    // else
+                    // {
+                    //     clearTextLine(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5));
+                    //     mvprintw(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5), "You already have a password entered for %s, try removing it or updating it instead.", tempKey);
+                    // }
+                // }
+
+                clearTextLine(((maxY/6)*2) , (((maxX/6) * 2) - 5));
+                for(int i = 1; i < 6; i++)
+                {
+                    makeMainMenuOptions(maxX, maxY, i, menuOptions[i - 1]);
+
+                }
+            }
+            else if(y == tempMenuNumb[2])
+            {
+                echo();
+                clearMainMenu(maxX, maxY);
+                // passwordCheckerMainMenu(masterPassword, passwordCheck, maxX, maxY, menuOptions);
+                // if((passwordCheckerMainMenu(masterPassword, passwordCheck, maxX, maxY, menuOptions)) == 1)
+                // {
+                    memset(tempPass, 0, 256);
+                    memset(tempKey, 0, 256);
+                    clearTextLine((((maxY/3)*2) + 2),0);
+                    mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 5), "%s","enter clientID you'd like to delete from the list. : ");
+                    getstr(tempID);
+                    // if(lookupData(hashTable, tempKey) != NULL)
+                    // {
+                        clearTextLine(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5));
+                        deleteHeapNode(heap, tempID);
+                        // removeData(hashTable, tempKey);
+                        mvprintw(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5), "client successfully deleted from system.");
+                        // passwordVaultSize--;
+
+                    // }
+                    // else
+                    // {
+                        // clearTextLine((((maxY/3)*2) + 2),0);
+                        // mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 5), "%s","you don't seem to have any accounts for that site/program...");
+                    // }
+                // }
+
+                for(int i = 1; i < 6; i++)
+                {
+                    makeMainMenuOptions(maxX, maxY, i, menuOptions[i - 1]);
+
+                }
+                makeMainMenu(maxX, maxY);
+            }
+            else if (y == tempMenuNumb[3])
+            {
+                echo();
+                clearMainMenu(maxX, maxY);
+                // if((passwordCheckerMainMenu(masterPassword, passwordCheck, maxX, maxY, menuOptions)) == 1)
+                // {
+                    // passwordCheckerMainMenu(masterPassword, passwordCheck, maxX, maxY, menuOptions);
+                    memset(tempStr, 0, 256);
+                    clearTextLine((((maxY/3)*2) + 2),0);
+                    mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 6), "%s","what program/website is this password for? : ");
+                    getstr(tempStr);
+
+                    // if(lookupData(hashTable, tempStr) != NULL)
+                    // {
+                        clearTextLine((((maxY/3)*2) + 2),0);
+                        clearTextLine(((maxY/6)*2) , (((maxX/6) * 2) - 6));
+                        mvprintw((((maxY/6)*2) + 4) , (((maxX/6)*2)-3),"Found it! Your %s Password is %s", tempStr,(char*)lookupData(hashTable, tempStr));
+                    // }
+                    // else
+                    // {
+                        // clearTextLine((((maxY/3)*2) + 2),0);
+                        // mvprintw((((maxY/6)*2) + 5) , (((maxX/6) * 2) - 5),"\nSorry, but I couldn't find any password for your %s account...\n\n", tempStr);
+                    // }
+                // }
+                for(int i = 1; i < 6; i++)
+                {
+                    makeMainMenuOptions(maxX, maxY, i, menuOptions[i - 1]);
+
+                }
+                makeMainMenu(maxX, maxY);
+            }
+            else if(y == tempMenuNumb[4])
+            {
+                echo();
+                clearMainMenu(maxX, maxY);
+                // if((passwordCheckerMainMenu(masterPassword, passwordCheck, maxX, maxY, menuOptions)) == 1)
+                // {
+                    // passwordCheckerMainMenu(masterPassword, passwordCheck, maxX, maxY, menuOptions);
+                    memset(tempPass, 0, 256);
+                    memset(tempKey, 0, 256);
+                    clearTextLine((((maxY/3)*2) + 2),0);
+                    // if(passwordVaultSize > 0)
+                    // {
+                        mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 5),"what program/site account info do you want to to update? : ");
+                        getstr(tempKey);
+
+                        // if(lookupData(hashTable, tempKey) != NULL)
+                        // {
+                            clearTextLine((((maxY/3)*2) + 2),0);
+                            mvprintw(((maxY/6)*2) , (((maxX/6) * 2) - 5),"enter updated password into vault: ");
+                            getstr(tempPass);
+                            removeData(hashTable, tempKey);
+                            insertData(hashTable, tempKey, tempPass);
+                            clearTextLine((((maxY/3)*2) + 2),0);
+                            // mvprintw((((maxY/3)*2) + 2) , 0,"enter updated password into vault: ");
+
+                        // }
+                        // else
+                        // {
+                            // clearTextLine(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5));
+                            // mvprintw(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5),"\n\nYou haven't entered a password for %s yet, try adding one instead\n\n", tempKey);
+                        // }
+                    // }
+                    // else
+                    // {
+                        // clearTextLine(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5));
+                        // mvprintw(((maxY/6)*2) + 5 , (((maxX/6) * 2) - 5),"\n\nYou haven't entered any accounts to the vault, try doing that first..."); 
+                    // }
+                // }
+
+                for(int i = 1; i < 6; i++)
+                {
+                    makeMainMenuOptions(maxX, maxY, i, menuOptions[i - 1]);
+
+                }
+                makeMainMenu(maxX, maxY);
+            }
+            else if(y == tempMenuNumb[5])
+            {
+
                 fp = fopen(fileName, "w");
-    			printf("Exiting...\n");
+             printf("Exiting...\n");
                 fprintf(fo, "Client List (in Order Processed)\n\n");
                 printHeap(heap, fo, 1);
                 printHeap(heap, fp, 0);
                 deleteHeap(heap);
                 fclose(fp);
                 fclose(fo);
-    			exit(1);
-    		default:
-    			printf("enter a number between 1-6\n");
-    			break;
+             exit(1);
 
-    	} 
+                fclose(fp);
+
+                destroyTable(hashTable);
+                endwin();
+                break;
+            }
+            else
+            {
+                printf("butts");
+            }
+
+            noecho();
+            mvprintw(prevY, prevX,character);
+            move(prevY, prevX);
+
+        }
+
+        refresh();
+        c = getch();
     }
+    endwin();
+
+    // while(choice != 6)
+    // {
+
+    //     printf("\n1.add new client\n2.remove old client\n3.cycle through heap\n4.print list\n5.save\n6.exit\n\n/> ");
+    // 	scanf("%d", &choice);
+    // 	switch(choice)
+    // 	{
+    // 		case 1:
+    //             memset(tempID, 0, strlen(line));
+    //             memset(tempPriority, 0, strlen(line));
+    //             memset(tempSymptomCode, 0, strlen(line));
+    // 			printf("enter client ID: ");
+    //             scanf("%s", tempID);
+    //             printf("enter Priority (1-5): ");
+    //             scanf("%s", tempPriority);
+    //             printf("enter symptom code: ");
+    //             scanf("%s", tempSymptomCode);
+    //             insertHeapNode(heap, atoi(tempPriority), tempID, tempSymptomCode);
+    //             // printf("heapID: %d, ID: %s, Priority: ")
+    // 			break;
+    // 		case 2:
+    //             memset(tempID, 0, strlen(line));
+    // 			// printf("remove old client\n");
+    //             printf("enter client ID: ");
+    //             scanf("%s", tempID);
+    //             deleteHeapNode(heap, tempID);
+    // 			break;
+    // 		case 3:
+    // 			// printf("go through heap from top to bottom\n");
+    //             // if(cycle >= heap->initialSize)
+    //             // {
+    //                 cycleCheck = 0;
+    //             // }
+    //             while(cycleCheck != 3)
+    //             {
+    //                 printf("<(1):(2)> Exit(3)\n\n/> ");
+    //                 scanf("%d", &cycleCheck);
+    //             switch(cycleCheck)
+    //             {
+    //                 // printf("<(1):(2)> Exit(3)\n\n");
+    //                 // scanf("%d", &cycleCheck);
+    //                 case 1:
+    //                     cycle--;
+    //                     if(cycle < 0)
+    //                     {
+    //                         cycle = heap->initialSize - 1;
+    //                     }
+    //                     // printf("cycle: %d\n", cycle);
+    //                     printf("[%d/%zu]: Client ID: %s, Priority: %d, Symptom Code: %s\n", cycle + 1, heap->initialSize, heap->heapTable[cycle]->clientID, heap->heapTable[cycle]->priority, heap->heapTable[cycle]->symptomCode);
+    //                     // cycle--;
+    //                     break;
+    //                 case 2:
+    //                     cycle++;
+    //                     if(cycle > heap->initialSize - 1)
+    //                     {
+    //                         cycle = 0;
+    //                     }
+    //                     // printf("cycle: %d\n", cycle);
+    //                     printf("[%d/%zu]: Client ID: %s, Priority: %d, Symptom Code: %s\n", cycle + 1, heap->initialSize, heap->heapTable[cycle]->clientID, heap->heapTable[cycle]->priority, heap->heapTable[cycle]->symptomCode);
+    //                     // cycle++;
+    //                     break;
+    //                 case 3:
+    //                     break;
+    //                 default:
+    //                     break;
+    //             }
+    //         }
+    //             // printf("[%d/%zu]: Client ID: %s, Priority: %d, Symptom Code: %s\n", cycle + 1, heap->initialSize, heap->heapTable[cycle]->clientID, heap->heapTable[cycle]->priority, heap->heapTable[cycle]->symptomCode);
+    // 			// cycle++;
+    //             break;
+    // 		case 4:
+    // 			// printf("printing list\n");
+    //             printHeap(heap, fo, 2);
+    // 			break;
+    // 		case 5:
+    //             // char tempMaxorMin[256];
+    // 			printf("heaipfy\n");
+    //             reheapifyMin(heap, 0);
+    //             // Node * tempMaxorMin = getMaxorMin(heap);
+    //             // printf("temp: %s\n", tempMaxorMin->clientID);
+    //             // getMaxorMin(heap);
+
+    // 			break;
+    // 		case 6:
+    //             fp = fopen(fileName, "w");
+    // 			printf("Exiting...\n");
+    //             fprintf(fo, "Client List (in Order Processed)\n\n");
+    //             printHeap(heap, fo, 1);
+    //             printHeap(heap, fp, 0);
+    //             deleteHeap(heap);
+    //             fclose(fp);
+    //             fclose(fo);
+    // 			exit(1);
+    // 		default:
+    // 			printf("enter a number between 1-6\n");
+    // 			break;
+
+    // 	} 
+    // }
 
     return 0;
 }
