b4d932d489c5a0861a8dd42e01d1a7b13af3e66f
Dominick Hera <dominickhera@me.com>
2017-09-18 11:15:29 -0400

    gonna work on Q3 now, gonna check if the other two work later

--- year3/cis2460/a1/a1.r
+++ year3/cis2460/a1/a1.r
@@ -1,16 +1,29 @@
-# x <- diceSim(n)
-# freq(x)
 
 diceSim <- function(n)
 {
+	rollType <- c(0,0,0,0,0,0,0,0,0,0,0,0)
+	for(i in 1:n)
+	{
+		dice1 <- sample(c(1:6), 1)
+		dice2 <- sample(c(1:6), 1)
+		totalCount <- (dice1 + dice2)
 
+		rollType[totalCount] <- (rollType[totalCount] + 1)
+	}
+
+	return(rollType)
 
 }
 
 freq <- function(samples)
 {
 
+	x <- diceSim(samples)
 
+	for(i in 1:12)
+	{
+		x[i] <- (x[1]/samples)
+	}
 }
 
 runSim <- function(n, sim, conv = identity)
