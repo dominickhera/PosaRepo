eaa967cf82525a5a995c0df87117178353847b1b
Dominick Hera <dominickhera@me.com>
2017-09-28 14:13:35 -0400

    file parsed, time to actually go through it and figure out the contents

--- year3/cis2750/a1/src/CalendarParser.c
+++ year3/cis2750/a1/src/CalendarParser.c
@@ -23,6 +23,90 @@
 
 ErrorCode createCalendar(char* fileName, Calendar** obj)
 {
+    FILE *fp;
+    char line[256];
+    char lineStorage[256][500];
+    char * fileTypeCheck;
+    int count = 0;
+
+
+    //parsing into a string array
+    if(fileName != NULL && fileName[0] == '\0')
+    {
+        if((fileTypeCheck = strstr(fileName, ".ics")))
+        {
+            if((fp = fopen(fileName, "r")))
+            {
+                while(fgets(line, sizeof(line), fp) != NULL)
+                {
+                    if(line[strlen(line) - 1] == '\n')
+                    {
+                        line[strlen(line) - 1] = '\0';
+                    }
+                    strcpy(lineStorage[count], line);
+                    count++;
+                }
+
+                fclose(fp);
+            }
+            else
+            {
+                return INV_FILE;
+            }
+        }
+        else
+        {
+            return INV_FILE;
+        }
+    }
+    else
+    {
+        return INV_FILE;
+    }
+
+	/*
+
+	//allocating and assigning values to ical object
+
+    Calendar * temp = malloc(sizeof(Calendar));
+    temp->prodID = malloc(sizeof(prodID));
+    temp->version = version;
+    strcpy(temp->prodID, prodID);
+    */
+
+    /*
+
+    //allocating and assigning values to event object
+
+    Event * tempEvent = malloc(sizeof(Event));
+    tempEvent->UID = malloc(sizeof(tempEvent->UID));
+    strcpy(tempEvent->UID, UID);
+    */
+
+    /*
+
+    //allocating and assigning values to alarm object, might move into seperate function since it can be called multiple times(?) ithink 
+
+    Alarm * tempAlarm = malloc(sizeof(Alarm));
+    tempAlarm->action = malloc(sizeof(char)*200);
+    tempAlarm->trigger = malloc(sizeof(trigger));
+    strcpy(tempAlarm->action, action);
+    strcpy(tempAlarm->trigger, trigger);
+	*/
+
+	/*
+
+	//allocating information regarding property within an ical event(?) i think
+	//moving to seperate function since it can be called N amount of times provided types dont repeat
+
+	Property * tempProp = malloc(sizeof(Property));
+	tempProp->propName = malloc(sizeof(char)*200);
+	tempProp->propDescr = malloc(sizeof(propDescr));
+	strcpy(tempProp->propName, propName);
+	strcpy(tempProp->propDescr, propDescr);
+
+	*/
+
 
 }
 
