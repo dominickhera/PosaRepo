b4fdf56dfca6aea7939fedd367d2820c7039cfe9
dominickhera <dominickhera@me.com>
2017-10-02 02:44:48 -0400

    cme on

--- year3/cis2750/a1/src/CalendarParser.c
+++ year3/cis2750/a1/src/CalendarParser.c
@@ -301,33 +301,33 @@ ErrorCode createCalendar(char* fileName, Calendar** obj)
             // }
 
             strTokTime = strtok(DSTAMPStorage, "T Z");
-            for(int j = 0; j < strlen(lineStorage[i]); j++)
-            {
-                if(lineStorage[i][j] == 'T')
-                {
-                    j++;
+            // for(int j = 0; j < strlen(lineStorage[i]); j++)
+            // {
+            //     if(lineStorage[i][j] == 'T')
+            //     {
+            //         j++;
 
-                  	if(tempUTC == true)
-                  	{
-                  		while(lineStorage[i][j] != 'Z')
-                    	{
-                        otherTempStorage[tempCount] = lineStorage[i][j];
-                        tempCount++;
-                        j++;
-                    	}
-                  	}
-                  	else
-                  	{
-                  		while(lineStorage[i][j] != '\0' )
-                    	{
-                        otherTempStorage[tempCount] = lineStorage[i][j];
-                        tempCount++;
-                        j++;
-                    	}
-                  	}
-                }
-            }
-            // strTokDate = strtok(DSTAMPStorage, "Z");
+            //       	if(tempUTC == true)
+            //       	{
+            //       		while(lineStorage[i][j] != 'Z')
+            //         	{
+            //             otherTempStorage[tempCount] = lineStorage[i][j];
+            //             tempCount++;
+            //             j++;
+            //         	}
+            //       	}
+            //       	else
+            //       	{
+            //       		while(lineStorage[i][j] != '\0' )
+            //         	{
+            //             otherTempStorage[tempCount] = lineStorage[i][j];
+            //             tempCount++;
+            //             j++;
+            //         	}
+            //       	}
+            //     }
+            // }
+            strTokDate = strtok(NULL, "T Z");
             strcpy(tempTime, strTokTime);
             strcpy(tempDate, otherTempStorage);
 
