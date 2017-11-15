63fff50ce795312934e173a90845efcae46ea3ad
Dominick Hera <dominickhera@me.com>
2017-11-15 01:12:36 -0500

    testing on school server

--- year3/cis2750/a3/src/gui.py
+++ year3/cis2750/a3/src/gui.py
@@ -12,12 +12,12 @@ def donothing():
    button.pack()
 
 
-def sosPlsWork(root):
-	print("whoops")
+def aboutApp():
+	tkMessageBox.showinfo("iCalGUI About", "iCalGUI was created by Dominick Hera\nYou can find more of my work on dominickhera.com")
 
 
 root = Tk()
-root.title("iCalGui")
+root.title("iCalGUI")
 root.geometry('400x400')
 
 menubar = Menu(root)
@@ -32,20 +32,42 @@ filemenu.bind_all("<Control-x>", quit)
 filemenu.bind_all("<Control-s>", donothing)
 createmenu = Menu(menubar, tearoff=0)
 createmenu.add_command(label="Create Calendar", command=donothing)
-
-# createmenu.add_separator()
-
 createmenu.add_command(label="Create Event", command=donothing)
-# editmenu.add_command(label="Copy", command=donothing)
-# editmenu.add_command(label="Paste", command=donothing)
-# editmenu.add_command(label="Delete", command=donothing)
-# editmenu.add_command(label="Select All", command=donothing)
-
 menubar.add_cascade(label="Create", menu=createmenu)
 helpmenu = Menu(menubar, tearoff=0)
-# helpmenu.add_command(label="Help Index", command=donothing)
-helpmenu.add_command(label="About iCalGui...", command=donothing)
+helpmenu.add_command(label="About iCalGUI...", command=aboutApp)
 menubar.add_cascade(label="Help", menu=helpmenu)
+
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT,fill=Y)
+
+fileViewPanel = Listbox(root,height=15,yscrollcommand=scrollbar.set)
+fileViewPanel.insert(1, "Python")
+fileViewPanel.insert(2, "Perl")
+fileViewPanel.insert(3, "C")
+fileViewPanel.insert(4, "PHP")
+fileViewPanel.insert(5, "JSP")
+fileViewPanel.insert(6, "Ruby")
+
+fileViewPanel.pack(side=TOP,fill=BOTH)
+scrollbar.config(command=fileViewPanel.yview)
+
+scrollbar2 = Scrollbar(root)
+scrollbar2.pack(side=RIGHT,fill=Y)
+
+logPanel = Text(root,width=50,height=8,yscrollcommand=scrollbar2.set)
+logPanel.insert(INSERT, "1\n2\n3\n4\n5\n6\n7\n8\n")
+logPanel.insert(END, "9")
+# logPanel.pack(side=BOTTOM)
+logPanel.pack()
+logPanel.config(state=DISABLED)
+logPanel.tag_add("here", "1.0", "1.4")
+logPanel.tag_add("start", "1.8", "1.13")
+scrollbar2.config(command=logPanel.yview)
+
+clearButton = Button(root, text="Clear", command=donothing)
+clearButton.pack(side=BOTTOM)
+# clearButton.config(side=BOTTOM)
 
 root.config(menu=menubar)
 
