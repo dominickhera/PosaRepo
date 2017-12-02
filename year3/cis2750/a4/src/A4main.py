e1c7202de7bb4d29392faeef6bd1bfe0c57a2e4e
Dominick Hera <dominickhera@me.com>
2017-12-02 00:11:04 -0500

    this is a shot int he dark

--- year3/cis2750/a4/src/A4main.py
+++ year3/cis2750/a4/src/A4main.py
@@ -390,7 +390,7 @@ class main(object):
 		for r in cursor:
 			eventCount = eventCount + 1
 
-		statusString = ("Database has " + organizerCount + " organizers and " + eventCount +" events.\n")
+		statusString = ("Database has ", organizerCount, " organizers and ", eventCount," events.\n")
 		self.logPanel.insert(INSERT, statusString)
 		self.logPanel.grid(row=16,column=0, rowspan=1, columnspan=50)
 		self.logPanel.config(state=DISABLED)
