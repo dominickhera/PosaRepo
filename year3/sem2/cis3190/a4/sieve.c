edf0f37f58a8e2c8b779f895172d8d49d5b63fef
dominickhera <dominickhera@me.com>
2018-04-03 11:44:13 -0400

    Finished C version

--- year3/sem2/cis3190/a4/sieve.c
+++ year3/sem2/cis3190/a4/sieve.c
@@ -1,10 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
+#include <time.h>
 
 
-int main(int argc, char **argv)
+int main()
 {
 
-	return 0;
+    int upperlimitInt = 0;
+    printf("Enter the upper limit of prime numbers to calculate: ");
+    scanf("%d", &upperlimitInt);
+    int sieveArray[upperlimitInt];
+    int newSieveCount = 0;
+    int secondCount = 0;
+    FILE * fp;
+
+    clock_t initTime = clock();
+
+    for(int i = 2; i <= upperlimitInt; i++)
+    {
+        sieveArray[i] = i; 
+    }
+
+    for(int i = 2; i <= upperlimitInt; i++)
+    {
+        if(sieveArray[i] % 2*i == 0)
+        {
+            // printf("shit equals 0? %d\n", sieveArray[i]);
+
+            for(int k = i * 2; k <= upperlimitInt; k += i)
+            {
+                sieveArray[k] = 0;
+            }
+
+            for(int k = i * 3; k <= upperlimitInt; k += i)
+            {
+                sieveArray[k] = 0;
+            }
+        }
+
+    }
+    for(int i = 2; i <= upperlimitInt; i++)
+    {
+        if(sieveArray[i] % 3*i == 0 )
+        {
+
+            for(int k = i * 3; k <= upperlimitInt; k += i)
+            {
+                sieveArray[k] = 0;
+            }
+        }
+    }
+
+    for(int i = 2; i <= upperlimitInt; i++)
+    {
+
+        if(sieveArray[i] % 5*i == 0)
+        {
+            // printf("shit equals 0? %d\n", sieveArray[i]);
+
+            for(int k = i * 5; k <= upperlimitInt; k += i)
+            {
+                sieveArray[k] = 0;
+            }
+        }
+    }
+
+    for (int i = 2; i <= upperlimitInt; i++)
+    {
+        if(sieveArray[i] != 0)
+        {
+            newSieveCount++;
+        }
+        // printf("sieveArray[%d]: %d\n", i, sieveArray[i]);
+    }
+    int resultsArray[newSieveCount];
+
+    for(int i = 2; i <= upperlimitInt; i++)
+    {
+        if(sieveArray[i] != 0)
+        {
+            resultsArray[secondCount] = sieveArray[i];
+            secondCount++;
+        }
+    }
+    fp = fopen("primeNumberCOutput.txt", "w");
+    // char * output = malloc(sizeof(char) * 10000);
+    fprintf(fp, "results\n");
+
+    for(int i = 0; i < newSieveCount; i++)
+    {
+        fprintf(fp, "%d\n", resultsArray[i]);
+    }
+    fprintf(fp, "\n");
+    // fputs(output, fp);
+    fclose(fp);
+    // free(output);
+
+    clock_t finishTime = clock();
+
+    printf("Execution Time: %f seconds\n", (float)(finishTime - initTime)/CLOCKS_PER_SEC);
+
+    return 0;
 }
