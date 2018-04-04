73596b5d85d4c62e4403da0ecb04ad77d5fb064e
dominickhera <dominickhera@me.com>
2018-04-04 15:12:39 -0400

    im so tired

--- year3/sem2/cis3190/a4/sieve.f95
+++ year3/sem2/cis3190/a4/sieve.f95
@@ -4,7 +4,7 @@ program sieve
     implicit none
 
 !     insert primary program here
-    integer :: upperLimit
+    integer :: upperLimit, i, k
 !     character(len=1) :: TICTAC(3,3)
     integer, allocatable, dimension(:) :: sieveArray
 
@@ -21,32 +21,42 @@ program sieve
         sieveArray(i) = i
     end do
 
+write(*,*)"2 begin"
     do i = 2, upperLimit, 1
-        if((sieveArray(i) % (2*i)) == 0) then
+        write(*,*) sieveArray(i), 2*i, modulo(sieveArray(i), 2*i)
+!         if(mod(sieveArray(i), (2*i)) == 0) then
+        if(mod(sieveArray(i), 2*i) == 0) then
+            write(*,*) "hello"
             do k = i * 2,upperLimit, 1
-                sieveArray(k) = 0
+                write(*,*) k, i
+                sieveArray(k * 2) = 0
             end do
         end if
     end do
 
     do i = 2, upperLimit, 1
-        if(sieveArray(i) % 3*i .eq. 0) then
+        if(mod(sieveArray(i), 3*i) == 0) then
             do k = i * 3,upperLimit, 1
-                sieveArray(k) = 0
+                sieveArray(k*3) = 0
             end do
         end if
     end do
 
     do i = 2, upperLimit, 1
-        if(sieveArray(i) % 5*i .eq. 0) then
+        if(mod(sieveArray(i), 5*i) == 0) then
             do k = i * 5,upperLimit, 1
-                sieveArray(k) = 0
+                sieveArray(k*5) = 0
             end do
         end if
     end do
 
+    write(*,*) "results"
+
     do i = 2, upperLimit, 1
-        write(*,*) sieveArray(i)
+        if(sieveArray(i) /= 0) then
+            write(*,*) sieveArray(i)
+        endif
+        
     end do
 
 contains
