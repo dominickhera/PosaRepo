a40b445f427e96b3800aab0011344964426fe0ff
dominickhera <dominickhera@me.com>
2018-04-04 12:08:37 -0400

    starting work on fortran part

--- year3/sem2/cis3190/a4/sieve.f95
+++ year3/sem2/cis3190/a4/sieve.f95
@@ -4,7 +4,50 @@ program sieve
     implicit none
 
 !     insert primary program here
+    integer :: upperLimit
+!     character(len=1) :: TICTAC(3,3)
+    integer, allocatable, dimension(:) :: sieveArray
 
+
+    do
+        write(*,*) 'Enter the upper limit of prime numbers to calculate: '
+        read(*,*) upperLimit
+        if(upperLimit > 2) exit
+        write(*,*) 'Error, Please Try Again'
+    end do
+
+    allocate(sieveArray(2:upperLimit))
+    do i = 2, upperLimit, 1
+        sieveArray(i) = i
+    end do
+
+    do i = 2, upperLimit, 1
+        if((sieveArray(i) % (2*i)) == 0) then
+            do k = i * 2,upperLimit, 1
+                sieveArray(k) = 0
+            end do
+        end if
+    end do
+
+    do i = 2, upperLimit, 1
+        if(sieveArray(i) % 3*i .eq. 0) then
+            do k = i * 3,upperLimit, 1
+                sieveArray(k) = 0
+            end do
+        end if
+    end do
+
+    do i = 2, upperLimit, 1
+        if(sieveArray(i) % 5*i .eq. 0) then
+            do k = i * 5,upperLimit, 1
+                sieveArray(k) = 0
+            end do
+        end if
+    end do
+
+    do i = 2, upperLimit, 1
+        write(*,*) sieveArray(i)
+    end do
 
 contains
 ! enter subroutines here
