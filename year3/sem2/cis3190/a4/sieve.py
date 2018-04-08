48c712777d0b1f8e52cbfe27e18f03a8df5be52b
dominickhera <dominickhera@me.com>
2018-04-08 18:04:26 -0400

    figured out python issues , now C and Python are 100% complete

--- year3/sem2/cis3190/a4/sieve.py
+++ year3/sem2/cis3190/a4/sieve.py
@@ -1,109 +1,60 @@
 #!/usr/bin/python
 import array
+import time
 
 if __name__ == "__main__":
 	print "Enter the upper limit of prime numbers to calculate: "
 	userInput = raw_input()
 	n = int(userInput.strip())
-	print "the user entered " + userInput
+	# print "the user entered " + userInput
+
+	startTime = time.time()
 	sieveList = []
-	for num in range(2,n+1):
+	for num in range(2,n+2):
 		sieveList.append(num)
 
-	# for num in range(2,n-1):
-		
-	# 	# print sieveList[num]
-	# 	# print "hiya"
-	# 	if((sieveList[num] % 2*num) == 0):
-	# 		# print num
-	# 		for num2 in range (1, n-1):
+	i = 0
+	while(i < n - 1):
+		if((sieveList[i] % (2*(i+1))) == 0):
+			k = 0
+			while(k < n - 1):
 				
-	# 			if(num2 * 2 < n):
-	# 				# print sieveList[num2*2]
-	# 				sieveList[num2*2] = 0
-	# 				# print "changed"
-	# 				# print sieveList[num2*2]
-	# 				# print "to that ^"
-	# 				# print num2*2
-
-
-	i = 1
-	while(i < n):
-		if((sieveList[i-1] % (2*i)) == 0):
-			# print sieveList[i]
-
-		# i += 1
-			k = 2
-			while(k < n):
-				
-				if(k <= n-1 ):
-					# print  k
-					# print sieveList[k]
-					sieveList[k] = 0
-					# print "post k is "
-					# print sieveList[k]
-				# i += 1
+				if(k <= n-1 and sieveList[k-2] is not 2):
+					sieveList[k-2] = 0
 				k = i * 2
 				i += 1
-				# print "hello"
-				# print k
 		i += 1
 
 
-	i = 1
-	while(i < n):
-		if((sieveList[i-1] % (3*i)) == 0):
-	# # # 		# print sieveList[i]
-
-	# # # # 	# i += 1
-			k = 3
+	i = 0
+	while(i < n - 1):
+		if((sieveList[i] % (3*(i + 1))) == 0):
+			k = 0
 			while(k < n-1):
-	# 			# print "k is"
-	# 			# print k
-				if(k <= n-1 and k is not 3):
-					print "k is "
-					print k
-					print "sieve list at k is"
-					print sieveList[k]
-					sieveList[k] = 0
-	# # 				# print "post k is "
-	# # 				# print sieveList[k]
-				# i += 1
+				if(k <= n-1 and sieveList[k-2 is not 3]):
+					sieveList[k-2] = 0
 				k = i * 3
 				i += 1
-	# 			# print k
 		i += 1
 
-	i = 1
-	while(i < n):
-		if((sieveList[i-1] % (5*i)) == 0):
-	# 		# print sieveList[i]
-
-	# 	# i += 1
-			k = 5
-			while(k < n):
-				if(k < n-1 and k is not 5):
-					# print k
-					# print "is k"
-					# print i
-					# print "that is i"
-	# 				# print "fucker"
-					# print "sieve list at k is"
-					# print sieveList[k]
-					sieveList[k] = 0
-					# print "post k is "
-					# print sieveList[k]
-				# i += 2
+	i = 0
+	while(i < n - 1):
+		if((sieveList[i] % (5*(i + 1))) == 0):
+			k = 0
+			while(k < n-1):
+				if(k < n-1 and sieveList[k-2 is not 5]):
+					sieveList[k-2] = 0
 				k = i * 5
 				i += 1
-				# print k
 		i += 1
 
-	print "results:"
-	for num in range(0, n - 1):
+	fileOutput = open('primeNumberPythonOutput.txt', 'w')
+	fileOutput.write("results:\n")
+	for num in range(0, n - 3):
 		if (sieveList[num] != 0):
-			# print "at num"
-			# print num
-			# print "is this"
-			print sieveList[num]
+			fileOutput.write("%s\n" % sieveList[num])
+
+
+	fileOutput.close()
+	print ("execution time: %s seconds" % (time.time() - startTime))
 			
