663fc1ebc66d5e2472822fc94e57175d6e842937
dominickhera <dominickhera@me.com>
2018-04-08 16:51:59 -0400

    almost done with python part

--- year3/sem2/cis3190/a4/sieve.py
+++ year3/sem2/cis3190/a4/sieve.py
@@ -27,80 +27,83 @@ if __name__ == "__main__":
 	# 				# print num2*2
 
 
-	i = 2
-	while(i < n - 1):
-		if((sieveList[i] % (2*i)) == 0):
+	i = 1
+	while(i < n):
+		if((sieveList[i-1] % (2*i)) == 0):
 			# print sieveList[i]
 
 		# i += 1
 			k = 2
-			while(k <= n-1):
+			while(k < n):
 				
-				if(k < n-1):
+				if(k <= n-1 ):
+					# print  k
+					# print sieveList[k]
 					sieveList[k] = 0
-				
+					# print "post k is "
+					# print sieveList[k]
+				# i += 1
 				k = i * 2
 				i += 1
+				# print "hello"
 				# print k
 		i += 1
 
 
-	i = 2
-	while(i < n - 1):
-		if((sieveList[i] % (3*i)) == 0):
-			# print sieveList[i]
+	i = 1
+	while(i < n):
+		if((sieveList[i-1] % (3*i)) == 0):
+	# # # 		# print sieveList[i]
 
-		# i += 1
+	# # # # 	# i += 1
 			k = 3
-			while(k <= n-1):
-				print k
-				if(k < n-1 and k is not 3):
+			while(k < n-1):
+	# 			# print "k is"
+	# 			# print k
+				if(k <= n-1 and k is not 3):
+					print "k is "
+					print k
+					print "sieve list at k is"
+					print sieveList[k]
 					sieveList[k] = 0
+	# # 				# print "post k is "
+	# # 				# print sieveList[k]
 				# i += 1
 				k = i * 3
 				i += 1
-				# print k
+	# 			# print k
 		i += 1
 
-	i = 2
-	while(i < n - 1):
-		if((sieveList[i] % (5*i)) == 0):
-			# print sieveList[i]
+	i = 1
+	while(i < n):
+		if((sieveList[i-1] % (5*i)) == 0):
+	# 		# print sieveList[i]
 
-		# i += 1
+	# 	# i += 1
 			k = 5
-			while(k < n-1):
-				# print k
-				# print "lol"
+			while(k < n):
 				if(k < n-1 and k is not 5):
-					# print "fucker"
+					# print k
+					# print "is k"
+					# print i
+					# print "that is i"
+	# 				# print "fucker"
+					# print "sieve list at k is"
+					# print sieveList[k]
 					sieveList[k] = 0
-				# i += 1
+					# print "post k is "
+					# print sieveList[k]
+				# i += 2
 				k = i * 5
 				i += 1
 				# print k
 		i += 1
-	# for num in range(2,n - 1):
-	# 	# print sieveList[num]
-	# 	if((sieveList[num] % 3*num) == 0):
-			
-	# 		for num2 in range (num*3, n-1):
-	# 			print num2 * 3
-	# 			# if(num2 * 3 < n -1):
-	# 				# print sieveList[num2*3]
-	# 				# sieveList[num2*3] = 0
-	# # 				# print sieveList[num2*3]
-
-	# for num in range(0,n - 1):
-	# # 	# print sieveList[num]
-	# 	if((sieveList[num] % 5*num) == 0):
-			
-	# 		for num2  in range (4, n-1):
-	# 			if(num2 * 5 < n):
-	# 				sieveList[num2*5] = 0
 
 	print "results:"
 	for num in range(0, n - 1):
 		if (sieveList[num] != 0):
+			# print "at num"
+			# print num
+			# print "is this"
 			print sieveList[num]
 			
