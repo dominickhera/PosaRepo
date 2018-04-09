f74978fa29977bc0ecdbac7b77c8d343b41c8bb8
dominickhera <dominickhera@me.com>
2018-04-08 20:00:22 -0400

    finished fortran fully, time to do ada and cobol

--- year3/sem2/cis3190/a4/sieve.f95
+++ year3/sem2/cis3190/a4/sieve.f95
@@ -1,11 +1,10 @@
-
 program sieve
 
     implicit none
 
-!     insert primary program here
     integer :: upperLimit, i, k
-!     character(len=1) :: TICTAC(3,3)
+    real :: startTime, endTime, clockRate
+    character (len=10000000) :: fileName
     integer, allocatable, dimension(:) :: sieveArray
 
 
@@ -16,50 +15,56 @@ program sieve
         write(*,*) 'Error, Please Try Again'
     end do
 
+    call cpu_time (startTime)
+
     allocate(sieveArray(2:upperLimit))
     do i = 2, upperLimit, 1
         sieveArray(i) = i
     end do
 
-write(*,*)"2 begin"
     do i = 2, upperLimit, 1
-        write(*,*) sieveArray(i), 2*i, modulo(sieveArray(i), 2*i)
-!         if(mod(sieveArray(i), (2*i)) == 0) then
-        if(mod(sieveArray(i), 2*i) == 0) then
-            write(*,*) "hello"
-            do k = i * 2,upperLimit, 1
-                write(*,*) k, i
-                sieveArray(k * 2) = 0
+        if(mod(2*i, sieveArray(i)) == 0) then
+            do k = i * 2,upperLimit, 2
+                if(k < upperLimit) then
+                    sieveArray(k) = 1
+                end if
             end do
         end if
     end do
 
     do i = 2, upperLimit, 1
-        if(mod(sieveArray(i), 3*i) == 0) then
-            do k = i * 3,upperLimit, 1
-                sieveArray(k*3) = 0
+        if(mod(3*i, sieveArray(i)) == 0) then
+            do k = i * 3,upperLimit, 3
+                sieveArray(k) = 1
             end do
         end if
     end do
 
     do i = 2, upperLimit, 1
-        if(mod(sieveArray(i), 5*i) == 0) then
-            do k = i * 5,upperLimit, 1
-                sieveArray(k*5) = 0
+        if(mod(5*i, sieveArray(i)) == 0) then
+            do k = i * 5,upperLimit, 5
+                sieveArray(k) = 1
             end do
         end if
     end do
 
-    write(*,*) "results"
+
+    open(unit=upperLimit, file='primeNumberFortranOutput.txt', status='new', action='write')
+
+    write(upperLimit, *) "Results:"
 
     do i = 2, upperLimit, 1
-        if(sieveArray(i) /= 0) then
-            write(*,*) sieveArray(i)
+        if(sieveArray(i) /= 1) then
+            write(upperLimit,*) sieveArray(i)
         endif
         
     end do
 
+    close(upperLimit, status='keep')
+
+    call cpu_time(endTime)
+    write(*,*) "Execution Time: ", (endTime - startTime) , " Seconds"
+
 contains
-! enter subroutines here
 
 end program sieve
